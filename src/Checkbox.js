223d5a815a07bc7e0a2928e03eebc240f3b9ab78
dangduyen110899 <duyendt@fimo.edu.vn>
2020-06-29 11:54:27 +0700

    Sửa checkbox

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -5,11 +5,11 @@ export const Checkbox = props => {
   return (
     <li>
       <input
-      key={props.id}
-      onClick={props.handleCheckFavorite}
-      type="checkbox"
-      checked={props.isChecked}
-      value={props.value} /> {props.value}
+        key={props.id}
+        onClick={props.handleCheckFavorite}
+        type="checkbox"
+        checked={props.isChecked}
+        value={props.value} /> {props.value}
     </li>
   )
 }
